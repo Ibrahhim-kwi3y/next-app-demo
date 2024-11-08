9df15b3580f7bc3c29d59f333774d7986fd40d3a
Ibrahim Elkwaiey <55067366+Ibrahhim-kwi3y@users.noreply.github.com>
2024-11-08 23:20:43 +0200

    Update page.tsx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,22 +1,22 @@
-import React from "react";
+// import React from "react";
 
-const Docs = async ({ params }: { params: { slug?: string[] } }) => {
-    if (!params) {
-        // Await params to make sure it's fully loaded before accessing properties
-        await new Promise(resolve => setTimeout(resolve, 0));
-    }
+// const Docs = async ({ params }: { params: { slug?: string[] } }) => {
+//     if (!params) {
+//         // Await params to make sure it's fully loaded before accessing properties
+//         await new Promise(resolve => setTimeout(resolve, 0));
+//     }
 
-    if (params.slug?.length === 2) {
-        return (
-            <h1>
-                Blog For Category {params.slug[0]} for title {params.slug[1]}
-            </h1>
-        );
-    } else if (params.slug?.length === 1) {
-        return <h1>Blog For Category {params.slug[0]}</h1>;
-    } else {
-        return <h1>Blog Home Page</h1>;
-    }
-};
+//     if (params.slug?.length === 2) {
+//         return (
+//             <h1>
+//                 Blog For Category {params.slug[0]} for title {params.slug[1]}
+//             </h1>
+//         );
+//     } else if (params.slug?.length === 1) {
+//         return <h1>Blog For Category {params.slug[0]}</h1>;
+//     } else {
+//         return <h1>Blog Home Page</h1>;
+//     }
+// };
 
-export default Docs;
+// export default Docs;
