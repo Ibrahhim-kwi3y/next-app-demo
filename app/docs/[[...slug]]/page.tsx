043539b0d7be8f466e2b5253b381527ce7dd4797
Ibrahim Elkwaiey <55067366+Ibrahhim-kwi3y@users.noreply.github.com>
2024-11-08 23:00:41 +0200

    Update page.tsx

--- app/docs/[[...slug]]/page.tsx
+++ app/docs/[[...slug]]/page.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 
- const Docs=({params}: {params:{slug:string[]}})=>{
+ const Docs=async ({params}: {params:{slug:string[]}})=>{
     if (params.slug?.length===2){
     return(<h1>Blog For Category {params.slug[0]} for title{params.slug[1]}</h1>)
 }
